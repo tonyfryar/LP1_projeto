6372a6d95b91edeecb553add3aa406e5ddf32d06
tonyfryar <tonyfryar@gmail.com>
2016-11-10 19:11:05 -0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -8,7 +8,8 @@
 #include <fstream>
 #include <stdio.h> // PARA USAR O REMOVE();
 #include <dirent.h>
-#include <sys/time.h> // PARA USAR O gettimeofday() (MEDIDOR DE TEMPO)
+#include <chrono>
+//#include <sys/time.h> // PARA USAR O gettimeofday() (MEDIDOR DE TEMPO)
 //#include "analise.hpp"
 #include "basededados.hpp"
 //#include "busca.hpp"
@@ -25,14 +26,14 @@ int main(int argc, char* argv[]){
         cout << "\t\t>> <-r> <nome_arquivo> : para Remover arquivos na base de dados." << endl;
         //cout << "\t\t>> <-txt> : para inserir todos os arquivos da pasta na base de dados." << endl;
         //cout << "\t\t>> <-clr> : para remover todos os arquivos da pasta na base de dados." << endl;
-        //cout << "\t\t>> <-li> : para Listar os arquivos da base de dados, por data de Inserção." << endl;
-        cout << "\t\t>> <-la> : para Listar os arquivos da base de dados, Alfabeticamente." << endl;
-        //cout << "\t\t>> <-ld> : para Listar os arquivos da base de dados, por quantidade Decrescente de palavras." << endl;
-        cout << "\t\t>> <-OR> <nome_arquivo> : para Buscar palavras na base de dados." << endl;
+        cout << "\t\t>> <-lI> : para Listar os arquivos da base de dados, por ordem de inserção." << endl;
+        cout << "\t\t>> <-lA> : para Listar os arquivos da base de dados, alfabeticamente." << endl;
+        cout << "\t\t>> <-lT> : para Listar os arquivos da base de dados, por quantidade de palavras." << endl;
+        //cout << "\t\t>> <-OR> <nome_arquivo> : para Buscar palavras na base de dados." << endl;
         return 0;
     }
     if(strcmp(argv[1],"-i")==0){
-        char palavras[MAX_TAM][MAX_TAM];
+        //char palavras[MAX_TAM][MAX_TAM];
         if(argc == 2){
             cout << "Opção requer argumento(s)" << endl;
             /*cin >> argv[2];
@@ -46,13 +47,12 @@ int main(int argc, char* argv[]){
             }*/
         }
         else{
-            for(int i=2; i<argc; i++){
-                strcpy(palavras[i-2],argv[i]);
-            }
-            if(VerificarOpcoes(palavras)){
-                for(int i=0; i<argc-2;i++){
-                    InserirArquivos(palavras[i]); 
-                }
+            //for(int i=2; i<argc; i++){
+            //    strcpy(palavras[i-2],argv[i]);
+            //}
+            //if(VerificarOpcoes(argc,argv)){
+            for(int ii=2; ii<argc;ii++){
+            	InserirArquivos(argv[ii]); 
             }     
         }
     }
@@ -66,33 +66,36 @@ int main(int argc, char* argv[]){
             } 
         }
     }
-    if(strcmp(argv[1],"-la")==0){
-    	ListarArquivos();
+    if(strcmp(argv[1],"-lA")==0){
+        ListarOrdemAlfabetica();
     }
-    if(strcmp(argv[1],"-OR")==0){
+    if(strcmp(argv[1],"-lI")==0){
+        ListarOrdemInsercao();
+    }
+    if(strcmp(argv[1],"-lT")==0){
+        ListarOrdemDecrescenteDePalavras();
+    }
+    /*if(strcmp(argv[1],"-OR")==0){
         int j=0;
-        char palavras[MAX_TAM][MAX_TAM];
+        string palavra;
         int tipo = 1;
             
         if(argc == 2){
             cout << "Opção requer argumento(s)" << endl;
-            cin >> argv[2];
-            for(int i =2;i<argc;i++){
-        		strcpy(palavras[j], argv[i]);
-                j++;
-            }
+
             if(VerificarOpcoes(palavras)){
-            	BD_BuscarPalavras(bd,palavras, argc, tipo, 0);
+            	BuscarPalavra(arquivo, argv[ii]);
             }
         }
         else{
         	for(int i =2;i<argc;i++){
-        		strcpy(palavras[j], argv[i]);
-                j++;
+        		//strcpy(palavras[j], argv[2]);
+                //j++;
+                cout << "Buscando palavra "  << argv[i] << endl;
+                BuscarPalavra(argv[i]);
             }
-            if(VerificarOpcoes(palavras)){
-            	BD_BuscarPalavras(bd,palavras, argc, tipo, 0);
-            }
+            
         }
+    }*/
     return 0;
 }
